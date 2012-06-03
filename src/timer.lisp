b71b4e2b007779f8f59266b38814f590509c7b9b
Josh Marchán <sykopomp@sykosomatic.org>
2012-06-03 03:00:09 -0400

    Implemented timers.

--- src/timer.lisp
+++ src/timer.lisp
@@ -1,20 +1,26 @@
 (in-package #:hipocrite)
 
 (defun call-after (time function)
-  ;; TODO
-  )
-(defun cancel-timer (timer-ref)
-  ;; TODO
-  )
+  (let ((timer (trivial-timers:make-timer function)))
+    (trivial-timers:schedule-timer timer time)
+    timer))
+
+(defun call-interval (interval function)
+  (let ((timer (trivial-timers:make-timer function)))
+    (trivial-timers:schedule-timer timer 0 :repeat-interval interval)
+    timer))
+
+(defun cancel-timer (timer)
+  (trivial-timers:unschedule-timer timer))
+
 (defun send-after (time message &optional (actor (current-actor)))
-  ;; TODO
-  )
-(defun send-interval (time message &optional (actor (current-actor)))
-  ;; TODO
-  )
+  (call-after time (lambda () (send actor message))))
+
+(defun send-interval (interval message &optional (actor (current-actor)))
+  (call-interval interval (lambda () (send actor message))))
+
 (defun exit-after (time reason &optional (actor (current-actor)))
-  ;; TODO
-  )
-(defun kill-after (time reason &optional (actor (current-actor)))
-  ;; TODO
-  )
+  (call-after time (lambda () (exit reason actor))))
+
+(defun kill-after (time &optional (actor (current-actor)))
+  (call-after time (lambda () (kill actor))))
