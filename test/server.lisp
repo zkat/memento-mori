3fcf0108c851c0c9d973f2ad656d51d482026329
Josh Marchán <sykopomp@sykosomatic.org>
2012-06-04 01:10:44 -0400

    CAST demo.

--- test/server.lisp
+++ test/server.lisp
@@ -3,7 +3,9 @@
   (:import-from #:hipocrite.server
                 #:start
                 #:defcall
-                #:call))
+                #:call
+                #:defcast
+                #:cast))
 (cl:in-package #:hipocrite.test.server)
 
 (defclass arithmetic-server () ())
@@ -11,12 +13,17 @@
 (defcall + (server arithmetic-server) (&rest numbers)
   (values (apply #'+ numbers) t))
 
+(defcast be-happy (server arithmetic-server) (about)
+  (format t "~&I, the arithmetic-server (@~a), am SO HAPPY about ~s!~%"
+          (current-actor) about))
+
 (defun test-arithmetic-server ()
   (let ((actor (start (make-instance 'arithmetic-server)
                       :name 'test-server)))
     (spawn (lambda ()
+             (cast actor 'be-happy '(pie))
              (multiple-value-call
-                 #'format t "~&Number: ~a, Second value: ~a~%"
+                 #'format t "~&Number: ~a, Second value: ~a.~%"
                  (call actor '+ '(1 2 3 4 5)))
              (kill 'die actor))
            :debugp t)))
