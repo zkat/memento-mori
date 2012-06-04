31ee940ea3881547fb463aa86725b4d2fa3c5ea1
Kat Marchán <kzm@sykosomatic.org>
2012-06-03 20:25:49 -0400

    More progress with hipocrite.server.

--- src/server.lisp
+++ src/server.lisp
@@ -3,61 +3,103 @@
   (:export
    ;; API
    #:start
-   ;; server callbacks
+   #:enter-loop
+   ;; server protocol
    #:init
-   #:handle-call
-   #:handle-cast
-   #:handle-info
-   #:terminate))
+   #:on-call
+   #:on-cast
+   #:on-direct-message
+   #:terminate
+   ;; Convenience
+   #:defcall
+   #:defcast))
 (cl:in-package #:hipocrite.server)
 
+;;;
+;;; Server actor
+;;;
 (defgeneric init (driver))
-(defgeneric handle-call (driver request from))
-(defgeneric handle-cast (driver request))
-(defgeneric handle-info (driver info))
+(defgeneric on-call (driver name args))
+(defgeneric on-cast (driver name args))
+(defgeneric on-direct-message (driver message))
 (defgeneric terminate (driver reason))
 
-(defparameter +cast-marker+ (gensym "CAST"))
-(defparameter +call-marker+ (gensym "CALL"))
+(defun enter-loop (driver)
+  (init driver)
+  (unwind-protect
+       (loop for msg = (receive)
+          do (cond ((call-msg-p msg)
+                    (%handle-call-msg driver msg))
+                   #+nil
+                   ((cast-message-p msg)
+                    (handle-cast driver (cast-request msg)))
+                   (t
+                    (on-direct-message driver msg))))
+    (terminate driver (make-condition 'actor-exit
+                                      :actor (current-actor)
+                                      :reason :normal))))
 
 (defun start (driver &key linkp monitorp trap-exits-p
               (debugp *debug-on-error-p*))
-  (spawn (lambda (&aux terminatedp)
-           (init driver)
-           (handler-bind ((actor-exit (lambda (exit)
-                                        (unless terminatedp
-                                          (terminate driver exit))))
-                          (error (lambda (e)
-                                   (unless terminatedp
-                                     (terminate driver e)))))
-             (loop for msg = (receive)
-                do (cond ((call-message-p msg)
-                          (handle-call driver (call-request msg) (call-from msg)))
-                         ((cast-message-p msg)
-                          (handle-cast driver (cast-request msg)))
-                         (t
-                          (handle-info driver msg))))
-             (setf terminatedp t)
-             ;; WHY IS THIS UNREACHABLE?
-             (terminate driver (make-condition 'actor-exit
-                                               :actor (current-actor)
-                                               :reason :normal))))
+  (spawn (lambda () (enter-loop driver))
          :linkp linkp
          :monitorp monitorp
          :trap-exits-p trap-exits-p
          :debugp debugp))
 
-(defun call-message-p (msg)
-  (and (listp msg) (eq +call-marker+ (car msg))))
+;;;
+;;; Call
+;;;
+(defstruct call-reply monitor values)
+(defstruct call-msg name args caller monitor)
 
-(defun call-request (msg)
-  (second msg))
+(define-condition call-error (error) ())
+(define-condition callee-down (call-error) ())
+(define-condition call-timeout (call-error) ())
 
-(defun call-from (msg)
-  (third msg))
+(defun call (actor name args &key (timeout 5))
+  (let ((monitor (monitor actor)))
+    (send actor (make-call-msg :monitor monitor
+                               :caller (current-actor)
+                               :name name
+                               :args args))
+    ;; TODO - really need this selective receive for this.
+    (receive-cond (reply :timeout timeout :on-timeout (error 'call-timeout))
+      ((and (call-reply-p reply)
+            (eq (call-reply-monitor reply) monitor))
+       (demonitor monitor)
+       (values-list (call-reply-values reply)))
+      ((and (down-signal-p reply)
+            (eq monitor (down-signal-monitor reply)))
+       (error 'callee-down)))))
 
-(defun cast-message-p (msg)
-  (and (listp msg) (eq +cast-marker+ (car msg))))
+(defun %handle-call-msg (driver msg)
+  (let ((results (multiple-value-list
+                  (on-call driver
+                           (call-msg-name msg)
+                           (call-msg-args msg)))))
+    (when results
+      (send (call-msg-caller msg)
+            (make-call-reply
+             :monitor (call-msg-monitor msg)
+             :values results)))))
 
-(defun cast-request (msg)
-  (second msg))
+(defmacro defcall (name (server-var server-class)
+                   lambda-list &body body)
+  (let ((args-var (gensym "ARGS")))
+    `(progn
+       #+nil
+       (defun ,name (,server-var &rest args)
+         (call ,server-var ',name args ,@(when timeoutp `(:timeout ,timeout))))
+       (defmethod on-call ((,server-var ,server-class)
+                           (,(gensym "NAME") (eql ',name))
+                           ,args-var)
+         (flet ((,name ,lambda-list ,@body))
+           (apply #',name ,args-var))))))
+
+;;;
+;;; Cast
+;;;
+(defstruct cast-msg name args)
+(defun cast (actor name args)
+  (send actor (make-cast-msg :name name :args args)))
