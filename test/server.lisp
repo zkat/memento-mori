13a8659c144f26144a64f40ec621b668c32b8989
Josh Marchán <sykopomp@sykosomatic.org>
2012-06-04 01:37:54 -0400

    Demo of direct messages.

--- test/server.lisp
+++ test/server.lisp
@@ -5,7 +5,8 @@
                 #:defcall
                 #:call
                 #:defcast
-                #:cast))
+                #:cast
+                #:on-direct-message))
 (cl:in-package #:hipocrite.test.server)
 
 (defclass arithmetic-server () ())
@@ -17,11 +18,15 @@
   (format t "~&I, the arithmetic-server (@~a), am SO HAPPY about ~s!~%"
           (current-actor) about))
 
+(defmethod on-direct-message ((server arithmetic-server) message)
+  (format t "~&Got a direct, non-call/cast message: ~s~%" message))
+
 (defun test-arithmetic-server ()
   (let ((server (start (make-instance 'arithmetic-server)
                       :name :test-server)))
     (spawn (lambda ()
              (be-happy server pi)
+             (send server "This is a regular message.")
              (multiple-value-call
                  #'format t "~&Number: ~a, Second value: ~a.~%"
                  (add-numbers server 1 2 3 4 5))
