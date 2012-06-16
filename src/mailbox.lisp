cbed6c53314773979d5fa49259b81e2217547efe
Josh Marchán <sykopomp@sykosomatic.org>
2012-06-16 17:58:54 -0400

    Less dangerous timeout interrupts for selective-receive.

--- src/mailbox.lisp
+++ src/mailbox.lisp
@@ -31,22 +31,20 @@
   obj)
 
 (define-condition receive-timeout (error) ())
-(defun receive (mailbox &key timeout on-timeout)
-  (without-interrupts
-    (if timeout
-        (multiple-value-bind (value completedp)
-            (with-timeout timeout
-              (#+sbcl sb-sys:allow-with-interrupts
-               #-sbcl progn
-               (with-interrupts (do-receive mailbox))))
-          (cond (completedp value)
-                (on-timeout (funcall on-timeout))
-                (t (error 'receive-timeout))))
-        (#+sbcl sb-sys:allow-with-interrupts
-         #-sbcl progn
-         (with-interrupts (do-receive mailbox))))))
 
-;; Oy vey, how to do this while still being interrupt-safe? :\
+(defun receive (mailbox &key timeout on-timeout)
+  (if timeout
+      (multiple-value-bind (value completedp)
+          ;; NOTE - Unlike the timeout in selective-receive, this one is
+          ;; relatively benign because we only ever interrupt during a
+          ;; condition-wait, with everything else being safe and internal.
+          (with-timeout timeout
+            (do-receive mailbox))
+        (cond (completedp value)
+              (on-timeout (funcall on-timeout))
+              (t (error 'receive-timeout))))
+      (do-receive mailbox)))
+
 (defun do-receive (mailbox)
   (without-interrupts
     (bt:with-lock-held ((mailbox-lock mailbox))
@@ -61,51 +59,60 @@
 (defun selective-receive (mailbox test &key timeout on-timeout)
   (if timeout
       (multiple-value-bind (value callback)
-          (block timer-block
-            (let ((timer (trivial-timers:make-timer
-                          (lambda ()
-                            (return-from timer-block (values nil nil))))))
-              (trivial-timers:schedule-timer timer timeout)
-              (unwind-protect
-                   (do-selective-receive mailbox test)
-                (trivial-timers:unschedule-timer timer))))
+          (let* ((timed-out-p-box (make-array nil :initial-contents nil))
+                 (timer (trivial-timers:make-timer
+                         (lambda ()
+                           (bt:with-lock-held ((mailbox-lock mailbox))
+                             (setf (aref timed-out-p-box) t)
+                             (bt:condition-notify (mailbox-cond-var mailbox)))))))
+            (trivial-timers:schedule-timer timer timeout)
+            (unwind-protect
+                 (do-selective-receive mailbox test timed-out-p-box)
+              (trivial-timers:unschedule-timer timer)))
         (cond (callback (funcall callback value))
               (on-timeout (funcall on-timeout))
               (t (error 'receive-timeout))))
       (multiple-value-bind (value callback)
-          (#+sbcl sb-sys:allow-with-interrupts
-           #-sbcl progn
-           (with-interrupts (do-selective-receive mailbox test)))
+          (do-selective-receive mailbox test (make-array nil :initial-contents nil))
         (funcall callback value))))
 
-(defun do-selective-receive (mailbox test)
-  (without-interrupts
-    (bt:with-lock-held ((mailbox-lock mailbox))
-      (let ((q (mailbox-queue mailbox))
-            match
-            callback)
-        (loop until callback
-           do (let (rest)
-                (unwind-protect
-                     (loop for (item . current-rest) on (dequeue-all q)
-                        for maybe-callback = (#+sbcl sb-sys:allow-with-interrupts
-                                              #-sbcl progn
-                                              (with-interrupts (funcall test item)))
-                        do (setf rest current-rest)
-                        when maybe-callback
-                        do (setf match item
-                                 callback maybe-callback)
-                          (return)
-                        else
-                        do (enqueue item q))
-                  (mapcar (rcurry #'enqueue q) rest)))
-           unless callback
-           do (#+sbcl sb-sys:allow-with-interrupts
-               #-sbcl progn
-               (with-interrupts
-                 (bt:condition-wait (mailbox-cond-var mailbox) (mailbox-lock mailbox))))
-           else
-           do (return-from do-selective-receive (values match callback)))))))
+(defun do-selective-receive (mailbox test timed-out-p-box)
+  (bt:with-lock-held ((mailbox-lock mailbox))
+    (let ((q (mailbox-queue mailbox))
+          match
+          callback)
+      (loop until callback
+         do
+           (let (item itemp rest)
+             (unwind-protect
+                  (loop with maybe-callback
+                     for (current-item . current-rest) on (dequeue-all q)
+                     do (setf rest current-rest
+                              item current-item
+                              itemp t
+                              maybe-callback (funcall test item))
+                     when maybe-callback
+                     do (setf match current-item
+                              callback maybe-callback
+                              itemp nil)
+                       (return)
+                     else
+                     do (enqueue item q))
+               (when (and itemp (null callback))
+                 (enqueue item q))
+               (mapcar (rcurry #'enqueue q) rest)))
+         unless callback
+         do
+           ;; NOTE - I'm not sure this one actually ever does its thing.
+           (when (aref timed-out-p-box)
+             (return-from do-selective-receive
+               (values nil nil)))
+           (bt:condition-wait (mailbox-cond-var mailbox) (mailbox-lock mailbox))
+           (when (aref timed-out-p-box)
+             (return-from do-selective-receive
+               (values nil nil)))
+         else
+         do (return-from do-selective-receive (values match callback))))))
 
 (defmacro receive-cond ((value-var mailbox &key timeout on-timeout) &body clauses)
   `(selective-receive ,mailbox
