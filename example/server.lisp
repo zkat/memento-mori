514e719c2da70a1fb8c2a8975aca201b8930583f
Kat Marchán <kzm@sykosomatic.org>
2012-06-18 00:04:15 -0400

    Improvements to example/server.lis.

--- example/server.lisp
+++ example/server.lisp
@@ -8,7 +8,7 @@
 (defstruct example-server deferred-request deferred-value)
 
 (defmethod mori-srv:on-init ((server example-server))
-  (format t "~&Initializing ~a~%" server))
+  (mori-log:info "Initializing ~a." server))
 
 (defcall add-numbers (&rest numbers)
     (server example-server :server-form (mori:find-actor :test-server))
@@ -16,33 +16,33 @@
 
 (defcast be-happy (about)
     (server example-server)
-  (format t "~&I, ~a (@~a), am SO HAPPY about ~s!~%"
-          server (mori:current-actor) about))
+  (mori-log:info "I, ~a (@~a), am SO HAPPY about ~s!"
+                 server (mori:current-actor) about))
 
 (defcast please-die ()
     (server example-server)
   (mori-srv:exit-server-loop)
-  (format t "~&Unreachable code. This won't print.~%"))
+  (mori-log:info "Unreachable code. This won't print."))
 
 (defcall deferred-reply (value)
     (server example-server :request request)
   (setf (example-server-deferred-value server) value
         (example-server-deferred-request server) request)
   (mori-srv:defer-call-reply)
-  (format t "~&Unreachable code. This won't print.~%"))
+  (mori-log:info "Unreachable code. This won't print."))
 
 (defmethod mori-srv:on-message ((server example-server) message)
-  (format t "~&Got a direct, non-call/cast message: ~s~%" message)
+  (mori-log:info "Got a direct, non-call/cast message: ~s." message)
   (when-let ((request (example-server-deferred-request server))
              (value (example-server-deferred-value server)))
-    (format t "~&Have something to reply to. Request: ~a, Value: ~a.~%"
-            request value)
+    (mori-log:info "Have something to reply to. Request: ~a, Value: ~a."
+                   request value)
     (mori-srv:reply request value 1 2 3)
     (setf (example-server-deferred-value server) nil
           (example-server-deferred-request server) nil)))
 
 (defmethod mori-srv:on-shutdown ((server example-server) reason)
-  (format t "~&~a is shutting down because of ~a.~%" server reason))
+  (mori-log:info "~a is shutting down because of ~a." server reason))
 
 (defun test-example-server ()
   (let ((server (mori-srv:start-server
@@ -50,13 +50,14 @@
                  :name :test-server
                  :debugp t)))
     (mori:spawn (lambda ()
-                  (mori-timer:call-after 2 (lambda ()
-                                             (mori:send server "This is a regular message.")))
+                  (mori-timer:send-after 2 "This is a regular message."
+                                         :actor server)
                   (be-happy server pi)
                   (multiple-value-call
-                      #'format t "~&Number: ~a, Second value: ~a.~%"
+                      #'mori-log:info "Number: ~a, Second value: ~a."
                       (add-numbers 1 2 3 4 5))
-                  (format t "Deferred reply: ~a"
-                          (multiple-value-list (deferred-reply server 'block-me)))
+                  (mori-log:info
+                   "Deferred reply: ~a"
+                   (multiple-value-list (deferred-reply server 'block-me)))
                   (please-die server))
                 :debugp t)))
