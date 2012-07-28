43fb5339bcfede92593f5d8fab17477fb1c97071
Josh Marchán <sykopomp@sykosomatic.org>
2012-07-27 21:57:23 -0400

    No current use for this, but I find myself writing it regularly.

--- src/utils.lisp
+++ src/utils.lisp
@@ -3,7 +3,10 @@
   (:export
    #:compare-and-swap
    #:without-interrupts
-   #:with-interrupts))
+   #:with-interrupts
+   #:make-light-queue
+   #:lq-enq
+   #:lq-deq))
 (cl:in-package #:memento-mori.utils)
 
 (defmacro compare-and-swap (place old-value new-value)
@@ -36,3 +39,36 @@
   `(ccl:with-interrupts-enabled ,@body)
   #-(or ccl sbcl)
   (error "NOT SUPPORTED"))
+
+(defstruct light-queue
+  spinlock
+  (queue (cons nil nil)))
+
+(defun lock-q (lq)
+  (loop until (compare-and-swap (light-queue-spinlock lq) nil t)))
+
+(defun unlock-q (lq)
+  (setf (light-queue-spinlock lq) nil))
+
+(defmacro with-queue-lock ((lq) &body body)
+  (let ((q-var (gensym)))
+    `(let ((,q-var ,lq))
+       (unwind-protect
+            (progn
+              (lock-q ,q-var)
+              ,@body)
+         (unlock-q ,q-var)))))
+
+(defun lq-enq (obj lq)
+  (with-queue-lock (lq)
+    (let ((q (light-queue-queue lq)))
+      (if (null (car q))
+          (setf (cdr q) (setf (car q) (list obj)))
+          (setf (cdr (cdr q)) (list obj)
+                (cdr q) (cdr (cdr q))))
+      (car q))))
+
+(defun lq-deq (lq)
+  (with-queue-lock (lq)
+    (let ((q (light-queue-queue lq)))
+      (pop (car q)))))
